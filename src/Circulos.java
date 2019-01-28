c0ab4b22591b3e85d83d47db6da604cc7537202c
joao <joao182cx@gmail.com>
2019-01-28 15:25:29 -0200

    Correção no algortimo de repetição que cria os círculos. Adição de uma linha de tamanho de 10 pixels para comprovar a distância entre os círculos.

--- src/Circulos.java
+++ src/Circulos.java
@@ -1,4 +1,6 @@
+import java.awt.Color;
 import java.awt.Graphics;
+import java.security.SecureRandom;
 
 import javax.swing.JPanel;
 
@@ -11,12 +13,21 @@ public class Circulos extends JPanel{
 		
 		final int centroY = getHeight()/2 - 5;
 		final int centroX = getWidth()/2 - 5;
+		final SecureRandom aleatorio = new SecureRandom();
 		
-		for(int i = 0; i < 120; i+=10) {
+		g.setColor(Color.RED);
+		g.drawLine(centroX + 10, centroY + 5, centroX + 20, centroY + 5);
+		
+
+		
+		for(int i = 0; i < 100; i+=10) {
 			
-			g.drawOval(centroX - i, centroY - i, i, i);
+			g.setColor(new Color(aleatorio.nextInt(256), aleatorio.nextInt(256), aleatorio.nextInt(256)));
+			
+			g.drawArc(centroX - i, centroY - i, 10 + i * 2, 10 + i * 2, 0, 360);
 			
 		}
+		
 		
 	}
 	
